318b8e6c65715164f83051cede848678eb072f6b
LostWeb45 <kostik052005@gmail.com>
2025-02-04 15:28:18 +0300

    final

--- harrypotterreact/src/components/CharacterCard/index.jsx
+++ harrypotterreact/src/components/CharacterCard/index.jsx
@@ -7,11 +7,7 @@ function CharacterCard({ name, age, image, school, key }) {
 
       <div className="card-text" key={key}>
         <h3>{name}</h3>
-        <p>Actor: Emma Watson</p>
-        <p>Gender: female</p>
         <p>House: {school.name}</p>
-        <p>Wand core: dragon heartstring</p>
-        <p>Alive: yes </p>
       </div>
     </div>
   );
